d843e1fd5d989d365d520fa171e58f88f4cf2e44
ivanramirez-git <ivanrene10@gmail.com>
2024-08-10 16:23:42 -0500

    rutas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  template: '<router-outlet></router-outlet>',
+  styles: []
 })
 export class AppComponent {
   title = 'ivan-ramirez-angular-devsu';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { NotFoundComponent } from './ui/pages/not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    // HomeComponent
+    { path: '', component: AppComponent },
+
+    // NotFoundComponent
+    { path: '**', component: NotFoundComponent },
+];
